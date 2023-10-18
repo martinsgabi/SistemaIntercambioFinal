fb539ade177007cfddb8bfa8304a50708a5efd20
martinsgabi <gabi.martinssilva26@gmail.com>
2023-10-18 14:04:47 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto> //Gabriela
-    (options => options.UseSqlServer("Data Source=SP-1491009\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Gabriela
+    (options => options.UseSqlServer("Data Source=SP-1491009\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
 /*builder.Services.AddDbContext<Contexto> //Isabela
     (options => options.UseSqlServer("Data Source=SP-1491007\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Juliani
-    (options => options.UseSqlServer("Data Source=SP-1491017\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Juliani
+    (options => options.UseSqlServer("Data Source=SP-1491017\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Renan
-    (options => options.UseSqlServer("Data Source=SP-1491031\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Renan
+    (options => options.UseSqlServer("Data Source=SP-1491031\\SQLSENAI;Initial Catalog = SistemaIntercambioFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
 
 var app = builder.Build();
